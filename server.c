acea5b5aff7f14614ca16d621327d82806225c77
1728163@kiit.ac.in <Sonu@0510$1998>
2019-07-26 18:25:26 +0530

    Final files of CN Lab 2.

---
+++ server.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main() {
+    printf("\nServer Socket");
+    int fd, res;
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    printf("\nReturn value : %d", fd);
+    if (fd == -1)
+        printf("\nSocket not created");
+    else
+        printf("\nSocket created");
+
+
+    struct sockaddr_in x;
+    x.sin_family = AF_INET; //ipv4 env.
+    x.sin_port = ntohs(6000); //port - network to host short (long to short)
+    x.sin_addr.s_addr = inet_addr("127.0.0.1"); //invoke ip address to socket
+
+    res = bind(fd, (struct sockaddr *) &x, sizeof(x));
+
+    if (res == -1)
+        printf("\nBind error\n");
+    else
+        printf("\nBind successfully\n");
+}
